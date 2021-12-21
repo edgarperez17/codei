edbca83c34f8b7e00c246ab1b95106d476f36c43
Edgar Guzman Perez <ed.guzman@gruporamos.com>
2021-12-21 15:11:08 -0400

    First commit

---
+++ app/Views/pages/about.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis perspiciatis, optio dicta reprehenderit provident voluptatum, quod inventore ad laudantium ratione quas eveniet labore. Eius, vero. Quas exercitationem accusamus praesentium harum dicta. Odio voluptate enim veritatis tempore deleniti, fugiat magni architecto iste, ipsum asperiores sit. Error quibusdam a magnam labore similique.</h1>
+</body>
+</html>
---
+++ app/Views/pages/home.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque voluptatum ea dolore ad dolor deleniti totam veritatis asperiores impedit non, cupiditate voluptas, voluptate amet sequi at. Tempora, libero autem? Vel perferendis voluptas, aspernatur ipsa cumque, doloribus nihil tempora ex accusantium sunt rem et amet sapiente reiciendis maxime aperiam praesentium vero.</p>
+</body>
+</html>
